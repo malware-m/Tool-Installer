c1f104a8f27476d25a557c880bc55d25a68ec483
M@lware <cyberops1010@gmail.com>
2024-11-02 19:51:41 +0500

    Update php.php

--- php.php
+++ php.php
@@ -1,9 +1,2 @@
-<?php
-// Set the URL you want to redirect to
-$url = "http://10zgav1hm6av4ly8juj8uwsax13srji77.oastify.com"; // Change this to the desired URL
-
-// Redirect the user
-header("Location: $url");
-exit();
-?>
+ <script>alert(3)</script>
 
