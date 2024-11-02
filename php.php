d1ad7becf061444110463ef6ed656210e867d262
M@lware <cyberops1010@gmail.com>
2024-11-02 19:18:32 +0500

    Update php.php

--- php.php
+++ php.php
@@ -1,6 +1,6 @@
 <?php
 // Set the URL you want to redirect to
-$url = "https://webhook.site/88ef2517-a45e-4c87-aece-4aa947bf9dbf"; // Change this to the desired URL
+$url = "88ef2517-a45e-4c87-aece-4aa947bf9dbf.dnshook.site"; // Change this to the desired URL
 
 // Redirect the user
 header("Location: $url");
