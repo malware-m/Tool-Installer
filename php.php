edbc92cd19df853bb6ea46f69a3f068f7659ae0d
M@lware <cyberops1010@gmail.com>
2024-11-02 19:49:35 +0500

    Update php.php

--- php.php
+++ php.php
@@ -1,6 +1,6 @@
 <?php
 // Set the URL you want to redirect to
-$url = "88ef2517-a45e-4c87-aece-4aa947bf9dbf.dnshook.site"; // Change this to the desired URL
+$url = "http://10zgav1hm6av4ly8juj8uwsax13srji77.oastify.com"; // Change this to the desired URL
 
 // Redirect the user
 header("Location: $url");
